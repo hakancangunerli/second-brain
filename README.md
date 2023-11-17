456f0303d39f8b3ef24a348397fba19b0989abd8
john <33205097+johngunerli@users.noreply.github.com>
2023-11-17 12:38:27 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # second-brain
-second brain is a project that loads up pdfs to chromadb and allows you to chat with them.
+This Python application leverages state-of-the-art natural language processing (NLP) models and provides functionalities for processing and managing PDF files. Built with Streamlit, it offers an interactive web interface for ease of use.Second brain is a project that loads up pdfs to chromadb and allows you to chat with them.
+
+# Features
+NLP Model Integration: Utilizes transformers for advanced NLP tasks.
+PDF Processing: Capable of handling and extracting data from PDF files.
+Chroma Integration: Implements Chroma for efficient data persistence.
+Streamlit Web Interface: Provides a user-friendly web application for interacting with the tool.
+Requirements
+Python 3.x
+Streamlit
+PyPDF2
+Transformers library
+Chroma and related dependencies
--- main.py
+++ main.py
@@ -1,24 +1,26 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import streamlit as st
 import PyPDF2
 import io
+from langchain.vectorstores import Chroma
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 import chromadb
+import os 
+# Initialize Chroma for persistence
+persist_directory = "./chromadb"
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-# Initialize the Chroma Client
-chroma_client = chromadb.Client()
-collection_name = "my_collection"
+# try to load a chromadb vectordb, if you can't find it, tell the user to create one.
 
-# Attempt to get or create the collection
 try:
-    collection = chroma_client.get_collection(name=collection_name)
-    st.write(f"Collection '{collection_name}' retrieved successfully.")
+    st.write("Loading ChromaDB...")
+    vector_db = Chroma(persist_directory=persist_directory, embedding_function=embedding_function)
+    # check if the folder is empty, if it's empty tell them to add some files first.
+    if os.listdir(persist_directory) == []:
+        st.error("Please create a ChromaDB first.")    
 except Exception as e:
-    st.error(f"Failed to retrieve collection: {e}. Attempting to create a new collection.")
-    try:
-        collection = chroma_client.create_collection(name=collection_name)
-        st.write(f"Collection '{collection_name}' created successfully.")
-    except Exception as e:
-        st.error(f"Failed to create collection: {e}")
-
+    st.error(f"An error occurred: {e}")
+    st.error("Please create a ChromaDB first.")
 
 # Function to process PDF file and extract text
 def process_pdf(uploaded_file):
@@ -29,60 +31,56 @@ def process_pdf(uploaded_file):
         text_data += pdf_reader.pages[page_num].extract_text()
     return text_data
 
+
 # Streamlit UI for file upload
 st.header("Upload to ChromaDB")
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 if uploaded_file is not None:
     # Extract text from the PDF file
     text_data = process_pdf(uploaded_file)
-    
+
     # Attempt to add the extracted text to the ChromaDB collection
     try:
-        collection.add(
-            documents=[text_data],
-            metadatas=[{"source": "pdf_upload"}],
-            ids=[uploaded_file.name]  # Use the file name as the unique ID
-        )
-        st.success(f"File '{uploaded_file.name}' successfully uploaded and processed!")
+        vector_db = Chroma(persist_directory=persist_directory, embedding_function=embedding_function)
+        
+        vector_db.add_texts([text_data])
+        
+        st.success(
+            f"File '{uploaded_file.name}' successfully uploaded and processed!")
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
-import streamlit as st
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
 
 # Function to generate answer using LLAMA
-def generate_answer_with_context(question, context):
-    from transformers import AutoTokenizer, TextStreamer
-    model_name = "Intel/neural-chat-7b-v3"    
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    input_tokens = str(question) + str(context)
-    inputs = tokenizer(input_tokens, return_tensors="pt").input_ids
-    streamer = TextStreamer(tokenizer)
+def generate_answer_with_context(user_input, context):
+    from ctransformers import AutoModelForCausalLM
+    llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF")
 
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
-    
-    return outputs
+    return llm(f"""
+                [INST]
+                <<SYS>>
+                 The following is your context, {context}
+                 Answer the question:
+            <</SYS>>
+                {user_input}[/INST]
+             """)
+
 
 # Streamlit UI for file upload and LLAMA querying
 
 # Functionality to query the collection and LLAMA
 st.header("Query Documents with LLAMA")
-query = st.text_input("Enter your query here:")
+question = st.text_input("Enter your query here:")
 if st.button("Search"):
     with st.spinner('Searching...'):
         # Query ChromaDB and retrieve documents
         # Assuming `collection.query` returns a list of dictionaries with 'text' key for documents
-        results = collection.query(
-            query_texts=[query],
-            n_results=5  # Assuming we want the top 5 results
-        )
-        
+        results = vector_db.similarity_search(question)
+
         if results:
             # For demonstration, we're taking just the top result for context
-            context = results
-            answer = generate_answer_with_context(query, context)
+            answer = generate_answer_with_context(
+                question, results[0].page_content)
             st.write(answer)
         else:
             st.write("No results found.")
